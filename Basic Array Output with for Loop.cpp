361ffbd7fdcbb48b0707a21a80c0259e5735f906
upadhyaytanmay07 <tanmayupadhyay1159@gmail.com>
2025-07-30 22:27:39 +0530

    Create Basic Array Output with for Loop.cpp

---
+++ Basic Array Output with for Loop.cpp
@@ -0,0 +1,27 @@
+/* Tanmay Upadhyay
+PRN 24070123120
+ENTC B2 */
+
+// basic code to print elements of array
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int arr[5]={10,30,40,40,50};
+    for(i=0; i<5; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+}
+
+/* output for this code
+
+10
+30
+40
+40
+50
+
+*/
