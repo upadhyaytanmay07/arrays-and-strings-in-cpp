395ae90ae534561b919a87fef42c47b6ffebb198
upadhyaytanmay07 <tanmayupadhyay1159@gmail.com>
2025-08-06 09:08:41 +0530

    Create concatenation of string.cpp

---
+++ concatenation of string.cpp
@@ -0,0 +1,33 @@
+/* Tanmay Upadhyay
+PRN 24070123120
+ENTC B2 */
+
+// concatenation of string
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str1;
+    string str2;
+    cout<<"Enter the string 1 : \n";
+    cin>>str1;
+    cout<<"\nEnter the string 2 : \n";
+    cin>>str2;
+    cout<<"\nConcatenating both strings\n";
+    cout<<str1+" "+str2;
+
+}
+
+/* output for this code
+
+Enter the string 1 : 
+Tanmay
+
+Enter the string 2 : 
+Upadhyay
+
+Concatenating both strings
+Tanmay Upadhyay
+
+*/
