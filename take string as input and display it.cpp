92f403799ccecdac8cc36e2023ca826035e8dbbd
upadhyaytanmay07 <tanmayupadhyay1159@gmail.com>
2025-08-06 09:03:52 +0530

    Create take string as input and display it.cpp

---
+++ take string as input and display it.cpp
@@ -0,0 +1,25 @@
+/* Tanmay Upadhyay
+PRN 24070123120
+ENTC B2 */
+
+//take string as input and display it 
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str;
+    cout<<"Enter the string \n";
+    cin>>str;
+    cout<<"\nThe string is : "<<str;
+
+}   
+
+/* output for this code 
+
+Enter the string 
+Tanmay
+
+The string is : Tanmay
+
+*/
